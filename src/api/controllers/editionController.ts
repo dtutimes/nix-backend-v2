5730787927e4916f12c826b3301fa7148f64050d
arpit <arpitmaurya1506@gmail.com>
2026-02-08 12:44:49 +0530

    fix: use res.locals.email to prevent crash on delete

--- src/api/controllers/editionController.ts
+++ src/api/controllers/editionController.ts
@@ -142,7 +142,7 @@ export const deleteEdition = asyncErrorHandler(async (req, res, next) => {
     return next(error);
   }
 
-  console.log("Edition deleted", edition, "by user", req.body.email);
+  console.log("Edition deleted", edition, "by user", res.locals?.email);
 
   res.status(StatusCode.OK).json({
     status: "success",
